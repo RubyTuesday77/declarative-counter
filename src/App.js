574846c0c0dc015e3cf0b7f0319662496b4faca3
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-10-26 21:39:46 -0400

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+// Step 1: Import useState from React:
+import React, { useState } from 'react'
+
+// Step 2-1: Define App function:
+function App() {
+  // Step 2-2: Step-2Invoke imported useState function, which returns variables: counter and changeCounter:
+  let [ counter, changeCounter ] = useState(1)
+  // Step 4: Make changeCounter a global variable so it can be invoked in dev tools:
+  window.changeCounter = changeCounter
+  // Step 3: Use JSX to interpolate JavaScript variable, counter, into the page:
+  return (
+    <div>
+      <h1>{ counter }</h1>
+      <h4>{ counter }</h4>
+    </div>
+  )
+}
+
+// Export:
+export default App;
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +45,4 @@ function App() {
 }
 
 export default App;
+*/
